9a0cce966c45d1f40b8b35170d94ddaeb5fec98d
lenorable <70905029+lenorable@users.noreply.github.com>
2022-10-13 14:19:51 +0200

    inputs regelen

--- scherm.py
+++ scherm.py
@@ -1,1 +1,14 @@
 # maak random choice of location possible
+from datetime import datetime
+import random
+
+datum_nu = datetime.today()
+tijd_nu = datetime.now()
+
+tijd = tijd_nu.strftime("%H:%M:%S")
+datum = datum_nu.strftime("%d/%m/%Y")
+gekozen_locaties = ["Den Haag","Utrecht","Zwolle"]
+
+bericht = input("laat een boodschap achter: ")
+gebruiker = input("naam of laat leeg: ")
+locatie = gekozen_locaties[random.randint(0,3)]
