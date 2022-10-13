aaa5f9fedb0fb4b74afef642bd88c84f9849a7c7
lenorable <70905029+lenorable@users.noreply.github.com>
2022-10-13 15:10:20 +0200

    zuil 1 eind

--- Zuil.py
+++ Zuil.py
@@ -4,11 +4,10 @@ import random
 
 datum_nu = datetime.today()
 tijd_nu = datetime.now()
+gekozen_locaties = ["Den Haag","Utrecht","Zwolle"]
 
 tijd = tijd_nu.strftime("%H:%M:%S")
 datum = datum_nu.strftime("%d/%m/%Y")
-gekozen_locaties = ["Den Haag","Utrecht","Zwolle"]
-
 bericht = input("laat een boodschap achter: ")
 gebruiker = input("naam of laat leeg: ")
 locatie = gekozen_locaties[random.randint(0,3)]
